013dcede1999ddb9a575a3f1d3c5d55a25a2891c
Arun Kumar Singh <arun161087@gmail.com>
2019-06-20 15:10:33 +0100

    File Created
    This is the file to change password in case of weblogic data sources via wlst.

---
+++ changePass.py
@@ -0,0 +1,32 @@
+#############################################################################
+# Author: Arun Kumar Singh
+# Purpose: Password update via wlst in weblogic data sources (Dev and Prod both)
+# Version: 0.2
+#############################################################################
+
+print("Password Change activity Started. Please make sure all backups have been taken")
+connect('weblogic','password_For_weblogic','t3://localhost:7001')
+print("Connected to Admin Server")
+cd('Servers/AdminServer')
+edit()
+startEdit()
+cd('JDBCSystemResources')
+print("Finding the list of datasources")
+dataSources=cmo.getJDBCSystemResources()
+
+for dataS in dataSources:
+  WDSName=dataS.getName();
+  print  'Changing Password & UserName for DataSource ', WDSName
+  cd('/JDBCSystemResources/'+WDSName+'/JDBCResource/'+WDSName+'/JDBCDriverParams/'+WDSName)
+  print('/JDBCSystemResources/'+WDSName+'/JDBCResource/'+WDSName+'/JDBCDriverParams/'+WDSName)
+  print("")
+  print ("Encrypting the password for Data Source")
+  encryptedPass = encrypt('password')
+  print("Encrypted Password value", encryptedPass)
+  set('PasswordEncrypted',encryptedPass)
+  print("")
+  print("Password has been changed for data source: ", WDSName)
+
+print("Activity finished, please verify the passwords.")
+save()
+activate()
